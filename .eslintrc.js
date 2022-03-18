d93b39db00033acdeff0a253425576a698168c29
李云飞 <liyunfei02@daojia-inc.com>
2022-03-18 23:29:29 +0800

    feat: ts

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    'vue/setup-compiler-macros': true,
+  },
+  globals: {
+    defineEmits: true,
+    document: true,
+    localStorage: true,
+    GLOBAL_VAR: true,
+    window: true,
+    defineProps: true,
+    defineExpose: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // 新增，必须放在最后面
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {},
+};
